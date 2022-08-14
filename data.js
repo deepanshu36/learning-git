f463c52125cef853349a23d6954bb3cd50578961
deepanshu <deepanshuk36@gmail.com>
2022-08-15 02:23:20 +0530

    intial commit

---
+++ data.js
@@ -0,0 +1,12 @@
+const data=[{"id":1,"first_name":"Agnese","last_name":"Sobtka","email":"asobtka0@ed.gov"},
+{"id":2,"first_name":"Hayes","last_name":"Geeritz","email":"hgeeritz1@vkontakte.ru"},
+{"id":3,"first_name":"Ignace","last_name":"Wolfindale","email":"iwolfindale2@artisteer.com"},
+{"id":4,"first_name":"Josie","last_name":"Coysh","email":"jcoysh3@google.com.br"},
+{"id":5,"first_name":"Maire","last_name":"McWhan","email":"mmcwhan4@domainmarket.com"},
+{"id":6,"first_name":"Belicia","last_name":"Terran","email":"bterran5@discuz.net"},
+{"id":7,"first_name":"Blondelle","last_name":"Dolton","email":"bdolton6@nymag.com"},
+{"id":8,"first_name":"Caye","last_name":"Dirr","email":"cdirr7@usnews.com"},
+{"id":9,"first_name":"Fayth","last_name":"McKelvie","email":"fmckelvie8@4shared.com"},
+{"id":10,"first_name":"Annora","last_name":"Bridywater","email":"abridywater9@gmpg.org"}]
+
+module.exports=data;
